a6384e510229be6d2b86aea50d4cce720ce897c0
Upendra Yadav <yupendra2000@gmail.com>
2023-08-23 19:00:30 +0530

    changes in upload_csv_to_mongodb.py because of duplicate data like emailid

--- upload_csv_to_mongodb.py
+++ upload_csv_to_mongodb.py
@@ -11,16 +11,22 @@ client = MongoClient(mongo_uri)
 db = client[db_name]
 collection = db[collection_name]
 
-# Read CSV and insert data into MongoDB
+# Read CSV and insert and also update if already inserted same data data into MongoDB
 with open('./data.csv', 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
+    
     for row in csv_reader:
         user_data = {
             "user": row["username"],
             "email": row["email"],
             "password": row["password"]
         }
-        collection.insert_one(user_data)
+        
+        # Define a query to find existing user by email
+        query = {"email": user_data["email"]}
+        
+        # Use update_one with upsert to handle duplicates
+        collection.update_one(query, {"$set": user_data}, upsert=True)
 
 # Close the MongoDB connection
 client.close()
